d02f8cb593fabbf57c47a03970bb97a12e1bb5e3
treethought <cpsweene@gmail.com>
2018-10-06 14:43:54 -0700

    Add hidden files aliases

---
+++ macos/aliases.zsh
@@ -0,0 +1,2 @@
+alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder"
+alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder"
