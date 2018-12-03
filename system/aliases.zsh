ae08a6b0f2e2e14a2070068d5c8724d72af9ceae
treethought <cpsweene@gmail.com>
2018-12-02 23:39:09 -0800

    update aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,7 +7,30 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
-fi# Clean up .DS_Store files
+fi
+
+
+# Always recursive and verbose
+alias cp="cp -rv"
+alias rm="rm -rv"
+alias mv="mv -v"
+
+# List all files colorized in long format
+alias l="ls -l -G"
+alias ls-a="ls -a -G"
+# List all files colorized in long format, including dot files
+alias la="ls -la -G"
+# List only directories
+alias lsd='ls -l -G | grep "^d"'
+# Always use color output for `ls`
+alias ls="command ls -G"
+
+# Process status
+alias psa="ps aux"
+alias psag="ps aux | ag "
+
+
+# Clean up .DS_Store files
 alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
 # File size
@@ -18,3 +41,4 @@ alias ka="killall"
 alias h="history"
 alias q="exit"
 
+alias findport='lsof -i tcp:'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,28 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias omz='subl ~/.oh-my-zsh'
-alias dotfiles='subl $DOTFILES'
+alias omz='e ~/.oh-my-zsh'
+alias dotfiles='e $DOTFILES'
 
 # Easy folder and file loading
 alias apps="cd /Applications"
 alias finder="open . -a finder.app"
 
-alias notes="e ~/notes"
-
-
+alias notes="e ~/org"
 
 # Network
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ and print $1'"
 alias whois="whois -h whois-servers.net"
+
+alias up='ping 8.8.8.8'
+
+alias falias="alias | grep"
+
+alias pyc='find . -name "*.pyc" -exec rm -rf {} \;'
+
+alias -g L='|less'
+alias -g T='|tail'
+alias -g G='|grep'
+alias -g V='|view -'
+
