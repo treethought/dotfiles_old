58d634cdd8d4f172efc91ad9acf417253f7d3931
treethought <cpsweene@gmail.com>
2020-12-10 17:46:41 -0800

    add doom config

---
+++ doom.d.symlink/config.el
@@ -0,0 +1,177 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, eonil
+;; clients, file templates and snippets.
+(setq user-full-name "Cam Sweeney"
+      user-mail-address "cameron.sweeney@ditoweb.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-gruvbox)
+(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+(doom-themes-treemacs-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+;; (use-package ewal
+;;     :init (setq ewal-use-built-in-always-p nil
+;;             ewal-use-built-in-on-failure-p t))
+
+;; (use-package ewal-doom-themes
+;; :config (progn
+;;           (load-theme 'ewal-doom-one t)
+;;                       (enable-theme 'ewal-doom-one)))
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type nil)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+;; dont autowrap long lines
+(setq truncate-lines nil)
+
+(setq pipenv-with-projectile 1)
+
+;; (after! lsp-ui
+;;   (setq lsp-ui-doc-max-width 150
+;;         lsp-ui-doc-max-height 30
+;;         lsp-ui-doc-position 'at-point
+;;         lsp-ui-doc-enable t))
+
+(add-hook! 'doom-switch-buffer-hook (lambda ()
+                                      (pipenv-deactivate)
+                                      (pipenv-activate)
+                                      ))
+
+(after! company-terraform company-terraform-init)
+
+(add-hook! 'vue-mode-hook #'lsp!)
+
+(setq org-confirm-babel-evaluate t)
+
+
+(setq todoist-token "a9f42914a5542a8985257ad1c76dcc69d15cec4d")
+
+(setq org-directory "~/Dropbox/org")
+(use-package org-super-agenda
+  :after org-agenda
+  :init
+  (setq org-agenda-skip-scheduled-if-done t
+        org-agenda-skip-deadline-if-done t
+        org-agenda-include-deadlines t
+        org-agenda-block-separator nil
+        org-agenda-compact-blocks t
+        org-agenda-start-day nil ;; i.e. today
+        org-agenda-span 1
+        org-agenda-start-on-weekday nil)
+  (setq org-agenda-custom-commands
+        '(("c" "Super view"
+           ((agenda "" ((org-agenda-overriding-header "")
+                        (org-super-agenda-groups
+                         '((:name "Today"
+                            :time-grid t
+                            :date today
+                            :order 1)))))
+            (alltodo "" ((org-agenda-overriding-header "")
+                         (org-super-agenda-groups
+                          '((:log t)
+                            (:name "To refile"
+                             :file-path "refile\\.org")
+                            (:name "Next to do"
+                             :todo "NEXT"
+                             :order 1)
+                            (:name "Important"
+                             :priority "A"
+                             :order 6)
+                            (:name "Today's tasks"
+                             :file-path "journal/")
+                            (:name "Due Today"
+                             :deadline today
+                             :order 2)
+                            (:name "Scheduled Soon"
+                             :scheduled future
+                             :order 8)
+                            (:name "Overdue"
+                             :deadline past
+                             :order 7)
+                            (:name "Meetings"
+                             :and (:todo "MEET" :scheduled future)
+                             :order 10)
+                            (:discard (:not (:todo "TODO")))))))))))
+  :config
+  (org-super-agenda-mode))
+
+(setq org-super-agenda-header-map (make-sparse-keymap))
+
+(defun my-restart-lsp-for-pipenv ()
+  "Restarts LSP server, to be used after pipenv activated"
+  (lsp-workspace-restart)
+  (lsp))
+
+(add-hook! pipenv-mode-hook #'my-restart-lsp-for-pipenv)
+
+
+(setq js2-basic-offset 2)
+(add-hook 'js2-mode-local-vars-hook
+          (lambda ()
+            (when (flycheck-may-enable-checker 'javascript-eslint)
+              (flycheck-select-checker 'javascript-eslint))
+            (setq +format-with eslint)
+            ))
+
+(setq jiralib-url "https://dito.atlassian.net")
+;; luybFUVL3DkPEoZngU704177
+;;
+(setq flycheck-disabled-checkers '(yaml))
+
+;; golang
+;; (setenv "GOPATH" "/home/cam/go")
+
+;; (setq lsp-ui-doc-enable t)
+;; (setq lsp-gopls-codelens nil)
+
+;; (package! yasnippet-snippets)
+
+(setq +format-on-save-enabled-modes
+      '(not yaml-mode  ; breaks helm templates
+            ))
+
+;; (setq lsp-yaml-schemas '("kubernetes"))
---
+++ doom.d.symlink/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" default))
+ '(package-selected-packages '(go-dlv protobuf-mode csharp-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
