7c414622291e257f2ac3a374282148ee6809f876
Cam Sweeney <cpsweene@gmail.com>
2023-08-05 16:47:08 -0700

    add vim-tidal

--- config.symlink/nvchad/init.lua
+++ config.symlink/nvchad/init.lua
@@ -4,10 +4,18 @@
 vim.opt.swapfile = false
 
 -- close quickfix menu after selecting choice
-vim.api.nvim_create_autocmd(
-  "FileType", {
-  pattern={"qf"},
-  command=[[nnoremap <buffer> <CR> <CR>:cclose<CR>]]})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "qf" },
+	command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
+})
+
+-- set commentstring for tidal
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "tidal" },
+	callback = function()
+		vim.bo.commentstring = "--%s"
+	end,
+})
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
